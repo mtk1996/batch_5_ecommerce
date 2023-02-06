216a7677f2105d544a26bc86afc3bc7fae86475d
Myo Thant Kyaw <myothantkyaw201021@gmail.com>
2023-02-06 19:14:23 +0630

    dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,12 +3,99 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Income;
+use App\Models\Order;
+use App\Models\Outcome;
+use App\Models\Product;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function show()
     {
-        return view('admin.dashboard');
+        $userCount = User::count();
+        $currentDay = date('d');
+        $currentMonthh = date('m'); //int
+        $currentYear = date('Y');
+        $currentMonth = date('F');
+
+        $monthArr = [$currentMonth];
+
+        $last6Day = [$currentDay];
+        $last6DayArr = [
+            [
+                'year' => $currentYear,
+                'month' => $currentMonthh,
+                'day' => $currentDay,
+            ]
+        ];
+
+
+        $yearMonth = [
+            ['year' => $currentYear, 'month' => $currentMonthh]
+        ];
+
+
+        for ($i = 1; $i <= 5; $i++) { // 1    2    3 ...  5
+            $monthArr[] =  date('F', strtotime("-$i month")); //1 feb   2  jan    3Dec
+
+            $yearMonth[] = [
+                'year' => date('Y', strtotime("-$i month")),
+                'month' => date('m', strtotime("-$i month")),
+            ];
+
+            $last6Day[] = date('d', strtotime("-$i day"));
+
+            $last6DayArr[] = [
+                'year' => date('Y', strtotime("-$i day")),
+                'month' =>  date('m', strtotime("-$i day")),
+                'day' => date('d', strtotime("-$i day")),
+            ];
+        }
+
+
+
+        $orderCount = [];
+        foreach ($yearMonth as $ym) {
+            $orderCount[] = Order::whereYear('created_at', $ym['year'])->whereMonth('created_at', $ym['month'])->count();
+        }
+
+        $totalIncome = [];
+        $totalOutcome = [];
+        foreach ($last6DayArr as $d) {
+            $totalIncome[]  =   Income::whereYear('created_at', $d['year'])->whereMonth('created_at', $d['month'])->whereDay('created_at', $d['day'])->sum('amount');
+            $totalOutcome[]  =   Outcome::whereYear('created_at', $d['year'])->whereMonth('created_at', $d['month'])->whereDay('created_at', $d['day'])->sum('amount');
+        }
+
+
+
+
+
+        /*
+            Income Outcome Sum Price
+        */
+        $totalIncomeCount = Income::whereYear('created_at', $currentYear)
+            ->whereMonth('created_at', $currentMonth)
+            ->whereDay('created_at', $currentDay)
+            ->sum('amount');
+        $totalOutcomeCount = Outcome::whereYear('created_at', $currentYear)
+            ->whereMonth('created_at', $currentMonth)
+            ->whereDay('created_at', $currentDay)
+            ->sum('amount');
+
+        $totalProductCount =  Product::count();
+
+        return view('admin.dashboard', compact(
+            'userCount',
+            'totalIncomeCount',
+            'totalOutcomeCount',
+            'totalProductCount',
+            'monthArr',
+            'orderCount',
+            'last6Day',
+            'totalIncome',
+            'totalOutcome',
+        ));
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,7 +1,101 @@
 @extends('admin.layout.master')
 
 @section('content')
-<h1>admin dashboard</h1>
-{{auth()->guard('admin')->user()}}
-<button class="btn btn-success">tet</button>
+<div class="row">
+    <div class="col-12">
+        <div class="d-flex justify-content-around">
+            <div class="card bg-primary p-3 d-flex flex-direction-row">
+                <h2 class="text-white text-center">User Count</h2>
+                <div class="text-white text-center">
+                    {{$userCount}}
+                </div>
+            </div>
+            <div class="card bg-primary p-3 d-flex flex-direction-row">
+                <h2 class="text-white text-center">Today Income</h2>
+                <div class="text-white text-center">
+                    {{$totalIncomeCount}}
+                </div>
+            </div>
+            <div class="card bg-primary p-3 d-flex flex-direction-row">
+                <h2 class="text-white text-center">Today Outcome</h2>
+                <div class="text-white text-center">
+                    {{$totalOutcomeCount}}
+                </div>
+            </div>
+            <div class="card bg-primary p-3 d-flex flex-direction-row">
+                <h2 class="text-white text-center">Product Count</h2>
+                <div class="text-white text-center">
+                    {{$totalProductCount}}
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-6">
+        <div class="card card-body">
+            <canvas id="sale"></canvas>
+        </div>
+    </div>
+    <div class="col-6">
+        <div class="card card-body">
+            <canvas id="inout"></canvas>
+        </div>
+    </div>
+</div>
+@endsection
+
+@section('js')
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script>
+    const ctx = document.getElementById('sale');
+
+new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: @json($monthArr),
+    datasets: [{
+      label: '၆လစာအရောင်းစာရင်း',
+      data: @json($orderCount),
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+
+
+// income outcome chart
+const inout = document.getElementById('inout');
+
+new Chart(inout, {
+  type: 'bar',
+  data: {
+    labels:@json($last6Day),
+    datasets: [
+        {
+      label: '၆ရက်စာ ဝင်ငွေ  စာရင်း ',
+      data: @json($totalIncome),
+      borderWidth: 1
+    },
+    {
+      label: '၆ရက်စာ ထွက်ငွေ စာရင်း  ',
+      data: @json($totalOutcome),
+      borderWidth: 1
+    },
+]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+</script>
 @endsection
